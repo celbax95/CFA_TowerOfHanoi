4d3221dc0bc719abbfd0c9dc1818e111ada0b05f
celbax95 <loicmace95@gmail.com>
2020-04-02 16:44:20 +0200

    [MENU] Ajout du layout du menu

--- app/src/main/java/com/example/towerofhanoi/app/MainActivity.java
+++ app/src/main/java/com/example/towerofhanoi/app/MainActivity.java
@@ -1,16 +1,59 @@
 package com.example.towerofhanoi.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 import com.example.towerofhanoi.R;
+import com.example.towerofhanoi.fragment.FragmentMenu;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentMenu menu;
+
+    @SuppressWarnings("deprecation")
+    private void setLocale(Locale locale){
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+            configuration.setLocale(locale);
+        } else{
+            configuration.locale=locale;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            getApplicationContext().createConfigurationContext(configuration);
+        } else {
+            resources.updateConfiguration(configuration,displayMetrics);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+setLocale(new Locale("en"));
+
+
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        menu = new FragmentMenu();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.mainFrame, menu).commit();
     }
 }
---
+++ app/src/main/java/com/example/towerofhanoi/fragment/FragmentMenu.java
@@ -0,0 +1,25 @@
+package com.example.towerofhanoi.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.towerofhanoi.R;
+
+public class FragmentMenu extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.fragment_menu, null);
+
+        return v;
+    }
+}
