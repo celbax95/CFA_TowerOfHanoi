07f2a65c5820d05f87a6c8425e0ce527ca091793
celbax95 <loicmace95@gmail.com>
2020-04-03 11:37:13 +0200

    [SCORES] Ajout du layout scores

---
+++ app/src/main/java/com/example/towerofhanoi/fragment/FragmentScores.java
@@ -0,0 +1,39 @@
+package com.example.towerofhanoi.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.towerofhanoi.R;
+import com.example.towerofhanoi.app.MainActivity;
+
+public class FragmentScores extends Fragment {
+
+    MainActivity context;
+
+    ListView listDisksButtons;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.fragment_scores, null);
+
+        createListDisksButtons(v);
+
+        return v;
+    }
+
+    private void createListDisksButtons(View v) {
+        listDisksButtons = v.findViewById(R.id.scores_listView_disksButtons);
+
+    }
+
+}
